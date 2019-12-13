659b566996f58f353d95c5553f41b4c9fbfac68e
allekok <one@allekok.com>
2019-12-13 10:29:43 +0330

    Initial commit

---
+++ site/image/portraits/README.md
@@ -0,0 +1,3 @@
+pre.jpg is from: http://www.salampaveh.ir/50073/%d8%ad%d8%b3%d9%86-%d8%b2%db%8c%d8%b1%da%a9-%d9%87%d9%86%d8%b1%d9%85%d9%86%d8%af-%d8%aa%da%a9%d8%b1%d8%a7%d8%b1%d9%86%d8%a7%d9%be%d8%b0%db%8c%d8%b1-%db%8c%d8%ad%db%8c%db%8c-%d9%85%d8%b9/  
+
+Edited with Inkscape and GIMP.
---
+++ sw.js
@@ -0,0 +1,43 @@
+const version = "v6";
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+	caches.open(version).then(function(cache) {
+	    return cache.addAll([
+		'/zirek/sw.js',
+		'/zirek/index.html',
+		'/zirek/site/script.js?v3',
+		'/zirek/site/style.css?v1',
+		'/zirek/site/image/back.jpg',
+		'/zirek/site/image/portraits/1.svg',
+		'/zirek/site/image/portraits/1.jpg',
+		'/zirek/site/DroidNaskh-Regular.woff2',
+	    ]);
+	})
+    );
+});
+
+self.addEventListener('activate', function(event) {
+    var cacheWhitelist = [version];
+
+    event.waitUntil(
+	caches.keys().then(function(keyList) {
+	    return Promise.all(keyList.map(function(key) {
+		if (cacheWhitelist.indexOf(key) === -1) {
+		    return caches.delete(key);
+		}
+	    }));
+	})
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+	caches.match(event.request).then(function(resp) {
+	    return resp || fetch(event.request).then(function(response) {		
+		return response;
+	    });
+	}).catch(function() {
+	    return caches.match('/zirek/index.html');
+	})
+    );
+});
