105bd5ac0fb06e9bbd201f85a4bc68eb734bfb3a
Payam <payambapiri.97@gmail.com>
2020-06-09 14:44:48 +0430

    site/script.js: Remove 'm4a|m4v' suffixes

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ self.addEventListener('install', function(event) {
 		caches.open(version).then(function(cache) {
 			return cache.addAll([
 				'/zirek/sw.js',
-				'/zirek/site/script.js?v6',
+				'/zirek/site/script.js?v7',
 				'/zirek/site/style.css?v2',
 				'/zirek/site/image/back.jpg',
 				'/zirek/site/image/portraits/1.svg',
